aab6b696d600bae60137a32fe41815f170bb9386
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2020-01-06 15:16:37 -0500

    Add Program / Counts the number of individual words in a string

---
+++ Text/Count_Words_in_a_String.py
@@ -0,0 +1,17 @@
+# Counts the number of individual words in a string.
+#** For added complexity read these strings in from a text file and generate a summary.
+
+def count_words():
+     # read, open loop through words in file
+    with open('count_words.txt', "r") as f:
+        for line in f: 
+            for word in line.split():
+
+                totalWords = sum(1 for word in line.split()) 
+                return totalWords
+
+
+
+
+
+
