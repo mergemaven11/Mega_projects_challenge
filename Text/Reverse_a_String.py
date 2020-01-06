b96d7af2e899b1565500cf8d20fa909c77015744
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2020-01-06 12:35:28 -0500

    Enter a string and the program will reverse it and print it out

--- Text/Reverse_a_String.py
+++ Text/Reverse_a_String.py
@@ -1,2 +1,9 @@
 # Enter a string and the program will reverse it and print it out.
 
+def rev():
+    # Get user input and store in variable 
+    string = input('Please Enter a word: ')
+    # method merges all of the characters resulting from the reversed iteration into a new string
+    rev_string = ''.join(reversed(string))  
+    # Print the string
+    print(rev_string)
